62edfdc7aecd43165fe68f3a0da229aac00852d9
pablovicz <pablowoina2205@gmail.com>
2020-08-17 08:48:53 -0300

    Commit: initial

---
+++ public/scripts/addField.js
@@ -0,0 +1,21 @@
+document.querySelector('#add-time')
+    .addEventListener('click', cloneField)
+
+function cloneField() {
+    const fieldContainer = document.querySelectorAll('.schedule-item')
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true)
+    const fields = newField.querySelectorAll('input')
+
+    let fieldIsNotEmpty
+
+    fields.forEach(field => {
+        if (!field.value)
+            fieldIsNotEmpty = true
+        field.value = ''
+    })
+
+    if (fieldIsNotEmpty)
+        return false
+
+    document.querySelector('#schedule-items').appendChild(newField)
+}
---
+++ src/server.js
@@ -0,0 +1,20 @@
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses, pageSuccess } = require('./pages')
+
+const express = require('express')
+const server = express()
+
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true
+})
+
+server
+    .use(express.urlencoded({ extended: true }))
+    .use(express.static('public'))
+    .get('/', pageLanding)
+    .get('/study', pageStudy)
+    .get('/give-classes', pageGiveClasses)
+    .get('/success', pageSuccess)
+    .post('/success', saveClasses)
+    .listen(5500)
